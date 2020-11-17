32be26f7455140c88b10fde756fd403b0fe619d0
Anorak52 <siber56@yandex.ru>
2020-11-17 15:35:04 +0300

    Initial

---
+++ src/main/java/org/example/entity/Order.java
@@ -0,0 +1,81 @@
+package org.example.entity;
+
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "order")
+public class Order {
+
+    @Id
+    private Long id;
+    @Column
+    private String name;
+    @Column
+    private Date registrationData;
+    @Column
+    private Date creatureDate;
+    @Column
+    private String comment;
+    @Column(name = "created")
+    private Long idOfCreature;
+    @Column(name = "deleted")
+    private Long idOfRemoved;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getRegistrationData() {
+        return registrationData;
+    }
+
+    public void setRegistrationData(Date registrationData) {
+        this.registrationData = registrationData;
+    }
+
+    public Date getCreatureDate() {
+        return creatureDate;
+    }
+
+    public void setCreatureDate(Date creatureDate) {
+        this.creatureDate = creatureDate;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Long getIdOfCreature() {
+        return idOfCreature;
+    }
+
+    public void setIdOfCreature(Long idOfCreature) {
+        this.idOfCreature = idOfCreature;
+    }
+
+    public Long getIdOfRemoved() {
+        return idOfRemoved;
+    }
+
+    public void setIdOfRemoved(Long idOfRemoved) {
+        this.idOfRemoved = idOfRemoved;
+    }
+}
